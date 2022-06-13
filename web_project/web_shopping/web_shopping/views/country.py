58c295d1bc629dee1c64d389675aec63ac308cf4
ccarrenov <cristiancarrenovillarroel@gmail.com>
2022-06-13 18:18:28 -0400

    update paginator

--- web_project/web_shopping/web_shopping/views/country.py
+++ web_project/web_shopping/web_shopping/views/country.py
@@ -2,24 +2,40 @@ from django.shortcuts import render
 from web_shopping.persistence.models import Country
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-headers = ['id', 'country_name', 'alpha2_code', 'alpha3_code', 'numeric_country', 'country_detail']
+headers = ['id', 'country_name', 'alpha2_code',
+           'alpha3_code', 'numeric_country', 'country_detail']
 
-def load(request): 
+
+def load(request):
     countries = []
-
     try:
         paginator = Paginator(find_all(), 10)
-        page = request.GET.get('page', 1)        
+        page = request.GET.get('page', 1)
+        if int(page) < 1:
+            page = 1
         countries = paginator.page(page)
-    except PageNotAnInteger:
+        size = paginator.num_pages
+    except (PageNotAnInteger, ValueError):
+        print('page not an integer')
+        page = 1
         countries = paginator.page(1)
+        size = paginator.num_pages
     except EmptyPage:
+        print('empty page')
         countries = paginator.page(paginator.num_pages)
+        size = paginator.num_pages
+        page = size
 
-    return render(request, 'country.html', {'headers' : headers, 'countries' : countries, 'size': paginator.num_pages})
+    return render(request, 'country.html', {'headers': headers, 'countries': countries, 'size': size, 'options': load_options(size), 'page': page})
 
-def paginator():
-    return ''
+
+def load_options(size):
+    options = []
+    if size != None:
+        for i in range(1, size):
+            options.append(i)
+    return options
+
 
 def find_all():
     return Country.objects.all()
